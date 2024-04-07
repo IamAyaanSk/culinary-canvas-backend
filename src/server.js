0c15cf843232d69a5ed52e46a998586cc44309bb
Ayaan <63299939+IamAyaanSk@users.noreply.github.com>
2024-04-08 01:38:55 +0530

    Chores: Quick cleanup

--- src/server.js
+++ src/server.js
@@ -2,10 +2,28 @@ const express = require('express');
 const server = express();
 
 require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
+const hpp = require('hpp');
 const port = process.env.NODE_DOCKER_PORT;
 const knex = require('./db/knex');
 
 const redisClient = require('./db/redis');
+
+server.disable('x-powered-by');
+
+server.use(
+  cors({
+    origin: '*',
+  }),
+);
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.use(helmet());
+
+server.use(hpp());
 
 server.get('/', (req, res) => {
   res.send('Hello World!');
